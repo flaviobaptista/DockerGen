2d8a3095c928c1d56b75f0be6eaea64166800deb
Vitadev Technology <111927624+flaviobaptista@users.noreply.github.com>
2025-01-29 13:49:23 -0300

    add information
    add input

--- js/confidencialidade.js
+++ js/confidencialidade.js
@@ -2,12 +2,14 @@
 
 function gerarDoc() {
     let nome = document.getElementById("nome").value;
+    let empresa = document.getElementById("empresa").value;
     let nacionalidade = document.getElementById("nacionalidade").value;
     let estadoCivil = document.getElementById("estadoCivil").value;
     let profissao = document.getElementById("profissao").value;
     let cpf = document.getElementById("cpf").value;
     let local = document.getElementById("local").value;
     let data = document.getElementById("data").value;
+    let cnpj = document.getElementById("cnpj").value;
 
     const date = new Date(data);
     const diasDaSemana = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'];
@@ -18,12 +20,12 @@ function gerarDoc() {
     const ano = date.getUTCFullYear();
 
     const dataFormatada = `${diaSemana}, ${dia} de ${mes} de ${ano}`;
+    const cnpjFormatado = formatarCNPJ(cnpj);
 
     document.getElementById("dockgen1").innerHTML = `${nome}, ${nacionalidade}, ${estadoCivil}, ${profissao}, ${cpf}`;
     document.getElementById("dockgen2").innerHTML = `${local}, ${dataFormatada}`;
     document.getElementById("dockgen3").innerHTML = `<strong> Assinatura: <br> </strong>${nome}`;
-    document.getElementById("dockgen4").innerHTML = `<strong> Cartão de Todos, ${local} <br> CNPJ:</strong> 38.065.293/0001-68 <br><br>`;
-}
+    document.getElementById("dockgen4").innerHTML = `<strong> ${empresa} <br> CNPJ:</strong> ${cnpjFormatado} <br><br>`;}
 
 function mascaraCpf(i) {
     let v = i.value;
@@ -36,4 +38,13 @@ function mascaraCpf(i) {
     i.setAttribute("maxlength", "14");
     if (v.length === 3 || v.length === 7) i.value += ".";
     if (v.length === 11) i.value += "-";
+}
+
+function formatarCNPJ(cnpj) {
+    cnpj = cnpj.replace(/\D/g, ""); // Remove caracteres não numéricos
+    cnpj = cnpj.replace(/^(\d{2})(\d)/, "$1.$2");
+    cnpj = cnpj.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3");
+    cnpj = cnpj.replace(/\.(\d{3})(\d)/, ".$1/$2");
+    cnpj = cnpj.replace(/(\d{4})(\d)/, "$1-$2");
+    return cnpj;
 }
