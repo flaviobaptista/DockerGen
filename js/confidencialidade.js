cbdf04ec881a2848d0e303bc3842309bebdb2667
Vitadev Technology <111927624+flaviobaptista@users.noreply.github.com>
2025-01-29 13:34:09 -0300

    Add text
    Add informações

--- js/confidencialidade.js
+++ js/confidencialidade.js
@@ -1,4 +1,3 @@
-
 // GERAR O PDF DE CONFIDENCIALIDADE
 
 function gerarDoc() {
@@ -9,28 +8,32 @@ function gerarDoc() {
     let cpf = document.getElementById("cpf").value;
     let local = document.getElementById("local").value;
     let data = document.getElementById("data").value;
-    let dataFormatada = data.split('-').reverse().join('/');
 
+    const date = new Date(data);
+    const diasDaSemana = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'];
+    const mesesDoAno = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+    const diaSemana = diasDaSemana[date.getUTCDay()];
+    const dia = date.getUTCDate();
+    const mes = mesesDoAno[date.getUTCMonth()];
+    const ano = date.getUTCFullYear();
 
+    const dataFormatada = `${diaSemana}, ${dia} de ${mes} de ${ano}`;
 
+    document.getElementById("dockgen1").innerHTML = `${nome}, ${nacionalidade}, ${estadoCivil}, ${profissao}, ${cpf}`;
+    document.getElementById("dockgen2").innerHTML = `${local}, ${dataFormatada}`;
+    document.getElementById("dockgen3").innerHTML = `<strong> Assinatura: <br> </strong>${nome}`;
+    document.getElementById("dockgen4").innerHTML = `<strong> Cartão de Todos, ${local} <br> CNPJ:</strong> 38.065.293/0001-68 <br><br>`;
+}
 
-    document.getElementById("dockgen1").innerHTML = nome + ", " + nacionalidade + ", " + estadoCivil + ", " + profissao + ", " + cpf;
-    document.getElementById("dockgen2").innerHTML = local + " " + dataFormatada;
-    document.getElementById("dockgen3").innerHTML = "<strong> Assinatura: <br> </strong>" + nome;
-
-};
-
-function mascaraCpf(i){
-   
+function mascaraCpf(i) {
     let v = i.value;
     
-    if(isNaN(v[v.length-1])){ 
-       i.value = v.substring(0, v.length-1);
-       return;
+    if (isNaN(v[v.length-1])) { 
+        i.value = v.substring(0, v.length-1);
+        return;
     }
     
     i.setAttribute("maxlength", "14");
-    if (v.length == 3 || v.length == 7) i.value += ".";
-    if (v.length == 11) i.value += "-";
- 
- }
+    if (v.length === 3 || v.length === 7) i.value += ".";
+    if (v.length === 11) i.value += "-";
+}
